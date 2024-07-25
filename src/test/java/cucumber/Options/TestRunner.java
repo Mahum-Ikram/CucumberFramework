29afa0f73dad1afe69d4682cc8398500a73e2b91
Mahum-Ikram <mahum.ikram@10pearls.com>
2024-07-25 15:12:31 +0500

    Message

---
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -0,0 +1,30 @@
+package cucumber.Options;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import pojo.AddPlace;
+import pojo.Location;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/features", glue = { "stepDefinitions" })
+
+public class TestRunner {
+
+	
+
+
+
+}
